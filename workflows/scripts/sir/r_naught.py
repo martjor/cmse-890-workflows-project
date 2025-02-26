55424b523908072c9879be937926e5a291bc2f32
Joy <martjor@msu.edu>
2025-02-26 01:34:41 -0500

    ADD: heatmap visualization

--- workflows/scripts/sir/r_naught.py
+++ workflows/scripts/sir/r_naught.py
@@ -1,27 +1,22 @@
 import numpy as np 
 from scripts.utils.io import save_dict
 
-infected = np.load(snakemake.input[0])[:,1,:]
+infected = np.load(snakemake.input[0])[:,1,:].mean(axis=0)
 
 # Obtain peak of each epidemic
-peaks = infected.max(1)
+peak = infected.max()
 
-# Identify simulations where peak ocurred
-idx = peaks > 0.1
+if peak > 0.1:
+    results = {
+        'peak': float(peak),
+        'time': float(infected.argmax())
+    }
 
-# Measure time to the peak for simulations where peak ocurred
-time = infected[idx].argmax(1)
-
-# Calculate number of new cases at each time
-new_infected = np.diff(infected[idx], axis=1)
-
-# Calculate R0 as the ratio of new infected to current infected
-r0 = new_infected / infected[idx,:-1]
-
-results = {
-    'mean': float(r0.mean()),
-    'std': float(r0.std())
-}
+else:
+    results = {
+        'peak': -1,
+        'time': -1,
+    }
 
 save_dict(snakemake.output[0], results)
 
